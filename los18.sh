ce49a82f61c6a047dabfe55785544614e1867757
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2024-12-05 21:39:07 +0530

    Update los18.sh

--- los18.sh
+++ los18.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-#rm -rf .repo/local_manifests
+rm -rf .repo/local_manifests
 
 # Rom source repo
-#repo init -u https://github.com/LineageOS-Revived/android.git -b lineage-18.1 --git-lfs
+repo init -u https://github.com/LineageOS-Revived/android.git -b lineage-18.1 --git-lfs
 #echo "=================="
 #echo "Repo init success"
 #echo "=================="
 
 # Clone local_manifests repository
-#git clone -b los18 https://github.com/mitmac29/local_manifests .repo/local_manifests
+git clone -b los18 https://github.com/mitmac29/local_manifests .repo/local_manifests
 #echo "============================"
 #echo "Local manifest clone success"
 #echo "============================"
 
 # Sync the repositories
-#/opt/crave/resync.sh
+/opt/crave/resync.sh
 #echo "============================"
 
 # Set up build environment
 . build/envsetup.sh
-echo "====== Envsetup Done ======="
+#echo "====== Envsetup Done ======="
 
 export DEVICE_MAINTAINER=MiTeSH
 export LINEAGE_MAINTAINER=MiTeSH
 export TZ=Asia/Dhaka
 export WITH_GMS_MINIMAL=true
-echo "======== Export Done ======="
+#echo "======== Export Done ======="
 
 # Lunch
 breakfast lavender
-echo "============="
+#echo "============="
 
 # Install clean
 # make installclean 
