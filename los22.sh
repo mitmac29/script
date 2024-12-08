0e3812cffcc5a8639b42e82dc3ed42540d0c8192
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2024-12-08 13:28:51 +0530

    Create los22.sh

---
+++ los22.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#rm -rf .repo/local_manifests
+
+# Rom source repo
+repo init -u https://github.com/2by2-Project/android.git -b 15 --git-lfs
+#echo "=================="
+echo "Repo init success"
+#echo "=================="
+
+# Clone local_manifests repository
+git clone -b los22 https://github.com/mitmac29/local_manifests .repo/local_manifests
+#echo "============================"
+echo "Local manifest clone success"
+#echo "============================"
+
+# Sync the repositories
+/opt/crave/resync.sh
+#echo "============================"
+
+# Set up build environment
+. build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+export DEVICE_MAINTAINER=MiTeSH
+export LINEAGE_MAINTAINER=MiTeSH
+export TZ=Asia/Dhaka
+export WITH_GMS_MINIMAL=true
+echo "======== Export Done ======="
+
+# Lunch
+lunch lineage_lavender-userdebug
+echo "============="
+
+# Install clean
+# make installclean 
+
+# Build rom
+m bacon
