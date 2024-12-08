5c51bb9cf56fde835d345a8a1c91b68ead0e6925
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2024-12-08 17:32:56 +0530

    Update los22.sh

--- los22.sh
+++ los22.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-rm -rf .repo/local_manifests
+#rm -rf .repo/local_manifests
 
 # Rom source repo
-repo init -u https://github.com/2by2-Project/android.git -b 15 --git-lfs
+#repo init -u https://github.com/2by2-Project/android.git -b 15 --git-lfs
 #echo "=================="
-echo "Repo init success"
+#echo "Repo init success"
 #echo "=================="
 
 # Clone local_manifests repository
-git clone -b los22 https://github.com/mitmac29/local_manifests .repo/local_manifests
+#git clone -b los22 https://github.com/mitmac29/local_manifests .repo/local_manifests
 #echo "============================"
-echo "Local manifest clone success"
+#echo "Local manifest clone success"
 #echo "============================"
 
 # Sync the repositories
-/opt/crave/resync.sh
+#/opt/crave/resync.sh
 #echo "============================"
 
 # Set up build environment
@@ -23,7 +23,7 @@ echo "Local manifest clone success"
 echo "====== Envsetup Done ======="
 
 # export
-export TARGET_BUILD_VARIANT=true 
+#export TARGET_BUILD_VARIANT=true 
 
 # Lunch
 breakfast lavender
@@ -33,4 +33,4 @@ echo "============="
 # make installclean 
 
 # Build rom
-#m bacon
+m bacon
