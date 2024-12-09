618fbebfd53e2a13b2547095178e871e6630c61f
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2024-12-10 00:43:48 +0530

    Create newlos.sh

---
+++ newlos.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests
+
+# Rom source repo
+repo init -u https://github.com/LineageOS-Revived/android.git -b lineage-18.1 --git-lfs
+#echo "=================="
+#echo "Repo init success"
+#echo "=================="
+
+# Clone local_manifests repository
+git clone -b newlos https://github.com/mitmac29/local_manifests .repo/local_manifests
+#echo "============================"
+#echo "Local manifest clone success"
+#echo "============================"
+
+# Sync the repositories
+/opt/crave/resync.sh
+#echo "============================"
+
+# Set up build environment
+. build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+#export DEVICE_MAINTAINER=MiTeSH
+#export LINEAGE_MAINTAINER=MiTeSH
+#export TZ=Asia/Dhaka
+#export WITH_GMS_MINIMAL=true
+#echo "======== Export Done ======="
+
+# Lunch
+breakfast lavender
+
+# Install clean
+# make installclean 
+
+# Build rom
+# m bacon
