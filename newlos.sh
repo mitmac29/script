20934fcb4bcbe4a358f2f13ba55de9feb8703f0a
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2024-12-10 12:15:38 +0530

    Update newlos.sh

--- newlos.sh
+++ newlos.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-rm -rf .repo/local_manifests
+#rm -rf .repo/local_manifests
 
 # Rom source repo
-repo init -u https://github.com/LineageOS-Revived/android.git -b lineage-18.1 --git-lfs
+#repo init -u https://github.com/LineageOS-Revived/android.git -b lineage-18.1 --git-lfs
 
 # Clone local_manifests repository
-git clone -b newlos https://github.com/mitmac29/local_manifests .repo/local_manifests
+#git clone -b newlos https://github.com/mitmac29/local_manifests .repo/local_manifests
 
 # Sync the repositories
-/opt/crave/resync.sh
+#/opt/crave/resync.sh
 
 # Set up build environment
 . build/envsetup.sh
