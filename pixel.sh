d0e57b82bb70488886cc8e45ec325ad4a8530a4a
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2024-12-12 04:23:51 +0530

    Create pixel.sh

---
+++ pixel.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests
+
+# Rom source repo
+repo init -u https://github.com/Project-PixelStar/manifest -b 14-qpr3 --git-lfs
+
+# Clone local_manifests repository
+git clone -b pixel https://github.com/mitmac29/local_manifests .repo/local_manifests
+
+# Sync the repositories
+/opt/crave/resync.sh
+
+# Set up build environment
+. build/envsetup.sh
+lunch pixelstar_lavender-userdebug
+# Lunch
+#mka bacon -jXT
