6d75be147068e3902914537fbd48ca65faa8c665
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2024-12-15 23:36:40 +0530

    Update omni.sh

--- omni.sh
+++ omni.sh
@@ -3,7 +3,7 @@
 rm -rf .repo/local_manifests
 
 # Rom source repo
-repo init -u https://github.com/mitmac29/android_ -b android-15 --git-lfs
+repo init -u https://github.com/mitmac29/android -b android-15 --git-lfs
 
 # Clone local_manifests repository
 git clone -b o https://github.com/miteshx07/local_manifests .repo/local_manifests
