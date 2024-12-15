3e088cac389c5697de52715ca7bdd29dac703a5a
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2024-12-15 15:05:09 +0530

    Create omni.sh

---
+++ omni.sh
@@ -0,0 +1,16 @@
+##!/bin/bash
+
+rm -rf .repo/local_manifests
+
+# Rom source repo
+repo init -u https://github.com/omnirom/android.git -b android-15 --git-lfs
+
+# Clone local_manifests repository
+git clone -b omni https://github.com/mitmac29/local_manifests .repo/local_manifests
+
+# Sync the repositories
+/opt/crave/resync.sh
+
+# Set up build environment
+. build/envsetup.sh
+brunch lavender
