e59e79392b4e373ebe6b5fe87015b72e2126ff50
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2025-01-31 20:16:22 +0530

    Update and rename clover.sh to infi.sh

--- clover.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source build/envsetup.sh
-
-lunch clover_lavender-ap4a-userdebug
-
-mka clover -j$(nproc --all)
---
+++ infi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+. build/envsetup.sh
+
+lunch infinity_lavender-userdebug
+
+mka bacon
