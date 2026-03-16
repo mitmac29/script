034c55c2cdf5c3fe8471f21a327ee03ba949dd52
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2026-03-16 15:37:34 +0530

    Update vos.sh

--- vos.sh
+++ vos.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-rm -rf .repo/local_manifests
+#rm -rf .repo/local_manifests
 
 # Rom source repo
-repo init -u https://github.com/VoltageOS/manifest.git -b 16.2 --git-lfs
-echo "=================="
-echo "Repo init success"
-echo "=================="
+#repo init -u https://github.com/VoltageOS/manifest.git -b 16.2 --git-lfs
+#echo "=================="
+#echo "Repo init success"
+#echo "=================="
 
 # Sync the repositories
-/opt/crave/resync.sh
-echo "============================"
+#/opt/crave/resync.sh
+#echo "============================"
 
-cd vendor/voltage-priv/keys && ./keys.sh && cd ../../..
+#cd vendor/voltage-priv/keys && ./keys.sh && cd ../../..
 
 # Clone local_manifests repository
-git clone -b vos https://github.com/mitmac29/android_device_xiaomi_lavender16 device/xiaomi/lavender
-echo "============================"
-echo "device tree clone success"
-echo "============================"
+#git clone -b vos https://github.com/mitmac29/android_device_xiaomi_lavender16 device/xiaomi/lavender
+#echo "============================"
+#echo "device tree clone success"
+#echo "============================"
 
 # Set up build environment
 . build/envsetup.sh
-echo "====== Envsetup Done ======="
+#echo "====== Envsetup Done ======="
 
 # Lunch
 brunch lavender user
-echo "======  Build Done ======="
+#echo "======  Build Done ======="
